faad40a71ca51869850712edc3568615b1a17f58
Adam Ergas <adamergas@yahoo.com>
2017-02-09 15:56:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+
+  def initialize(name = "This dog has no name. Sad.")
+    @name = name
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
