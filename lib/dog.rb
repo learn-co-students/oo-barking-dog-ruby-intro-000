242b56b75a6d3e37dd8105765850b249d9f8528c
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-19 17:30:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+end
+
+class Dog
+  def bark
+    puts "woof!"
+  end
+
+end
