14d9b59bc4e99030da426da9853708a2c4217e2a
lior Shine <shinelior@gmail.com>
2020-02-27 17:12:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+  attr_accessor(:name,:bark)
+  def initialize
+    @name = name
+    
+  end
+  def bark
+    puts "woof!"
+  end
+    
+  #def name=(the_dog_name)
+    #@name = the_dog_name 
+ # end
+    
+  
+end
