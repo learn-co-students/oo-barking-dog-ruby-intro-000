c066eed258177f52b1a04a2984cf19406703211e
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-24 17:30:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,49 @@
-# Your code goes here!
+
+class Dog 
+ def name=(dog_name) 
+   @name=dog_name
+ end
+
+ def name
+   @name
+ end 
+ def bark 
+   puts "woof!"
+ end
+ end
+
+
+
+
+=begin def name=(name,bark)
+  @name=name
+  @bark=bark
+end
+
+  def name(name,bark)
+  @name=name
+  @bark=bark
+end
+
+ end
+
+fido=Dog.new("Fido", "woof!")
+fido.name
+fodo.bark
+
+ def name=(dog_name)
+  @name=dog_name
+end
+def bark=(bark)
+  @bark="Woof!"
+end
+  
+
+end
+
+
+#fido=Dog.new
+fido.name="Fido"
+puts fido.name
+puts fido.bark
+=end
