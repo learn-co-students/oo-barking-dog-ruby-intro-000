0eddd790c5e2485069edaaa3323e8e85ad0fcab0
logicfirst <slee@logicfirstllc.com>
2020-10-27 20:34:06 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        return @name
+    end
+
+    def bark
+        puts "woof!"
+    end
+
+
+
+end
