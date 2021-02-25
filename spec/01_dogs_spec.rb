16b908725263a3a26d69752eae10a67bafb28c75
George Serna <sernageorge@gmail.com>
2021-02-25 03:15:16 +0000

    Done.

--- spec/01_dogs_spec.rb
+++ spec/01_dogs_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/dog'
+
 describe "Dog" do
   let(:fido) {Dog.new}  # Look up what let does in RSpec
   # https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
